7a83e8dd91805aff28745720923c5eec7d82a173
rohanpathak2000 <75062006+rohanpathak2000@users.noreply.github.com>
2021-11-22 16:16:46 +0530

    Minor Changes

--- main.R
+++ main.R
@@ -1,19 +1,18 @@
-getwd()
+rm(list=ls())
 setwd("C:/FallSem 21-22/CSE3505 - Data Analytics/Project Prep/Sentiment Analysis of IMDb reviews")
 data <- read.csv("movie_reviews.csv")
 #head(data)
 #View(data)
-names(data)
 pacman::p_load(tm)
 pacman::p_load(tmap)
 pacman::p_load(SnowballC)
 
-str(data)
-#corpus <- iconv(df$review, to="utf-8")
+# str(data)
+# corpus <- iconv(df$review, to="utf-8")
 # Converting the reviews into a corpus in which each review can be treated as a document. 
 corpus <- Corpus(VectorSource(data$review))
-inspect(corpus[1:5])
-corpus
+#inspect(corpus[1:5])
+#corpus
 
 # ---- Data Cleaning ------#
 
@@ -40,7 +39,6 @@ cleanset <- tm_map(cleanset,stemDocument)
 
 # Creates a document matrix which is a Bag-of-Words model with Term Frequency
 frequencies <- DocumentTermMatrix(cleanset)
-#findAssocs(frequencies, terms = findFreqTerms(frequencies, lowfreq = 50), corlimit = 0.25) 
 
 #head(df)
 #inspect(frequencies[1:20,1:20])
@@ -56,7 +54,6 @@ colnames(final_reviews) <- make.names(colnames(final_reviews))
 
 cols <- names(final_reviews)
 
-#head(df)
 # Removing words which have a frequency less than 2000 in all the reviews 
 for (col in cols) {
   if(sum(final_reviews[col]) < 2000) {
@@ -64,23 +61,16 @@ for (col in cols) {
   }
 }
 
-dim(final_reviews)
-
 final_reviews$sentiment <- data$sentiment
 
-head(df)
-
-View(final_reviews)
 pacman::p_load(e1071)
 pacman::p_load(caret)
 
 
 # Label encoding the categorical data
 final_reviews$sentiment <- ifelse(final_reviews$sentiment == "negative",0,1)
-final_reviews$sentiment <- factor(final_reviews$sentiment)
-
+#final_reviews$sentiment <- factor(final_reviews$sentiment)
  
-#View(final_reviews)
 
 #Analysis
 frequency <- colSums(as.matrix(final_reviews[,-dim(final_reviews)[2]]))                                  
@@ -96,6 +86,7 @@ barplot(freq[1:10],
         names.arg = words[1:10],
         col = "darkred"
 )
+
 pacman::p_load(caTools)
 
 set.seed(123)
@@ -110,99 +101,104 @@ features_test <- testing_data[,-dims[2]]
 labels_train <- training_set[,dims[2]]
 labels_test <- testing_data[,dims[2]]
 
-features_train_matrix <- data.matrix(training_set[,-dims[2]])
-features_test_matrix <- data.matrix(testing_data[,-dims[2]])
-labels_train <- training_set[,dims[2]]
-labels_train_matrix <- data.matrix(training_set[,dims[2]])
-labels_test <- testing_data[,dims[2]]
+features_train_matrix <- data.matrix(features_train)
+features_test_matrix <- data.matrix(features_test)
+labels_train_matrix <- data.matrix(labels_train)
 
 pacman::p_load(Metrics)
 ### Naive Bayes Classification ###
 clf <- naiveBayes(sentiment ~ .,  data = training_set, laplace = 1)
-dim(testing_data)
 #Predictions
 y_pred <- predict(clf, features_test)
-
-#Confusion Matrix
-confusion_matrix <- table(testing_data$sentiment,y_pred)
-confusion_matrix
-
 #Evaluation Metrics
 precision <- posPredValue(as.factor(y_pred), as.factor(testing_data$sentiment), positive="1")
 recall <- sensitivity(as.factor(y_pred), as.factor(testing_data$sentiment), positive="1")
-precision
-recall
 Accuracy <- accuracy(testing_data$sentiment,y_pred)
 Accuracy
+precision
+recall
 
-# Accuracy - 78.94%
+
+# Accuracy - 78.98%
 # Precision - 77.5%
-# Recall - 81.54%
+# Recall - 81.68%
 
 
 pacman::p_load(ROCR)
 pacman::p_load(glmnet)
+pacman::p_load(dplyr)
 
 #Logistic Regression with Lasso Regularization
-set.seed(123) 
 cv.lasso <- cv.glmnet(features_train_matrix, labels_train_matrix, alpha = 1, family = "binomial")
 model <- glmnet(features_train_matrix, labels_train_matrix, alpha = 1, family = "binomial",
                 lambda = cv.lasso$lambda.min)
-
-pacman::p_load(dplyr)
 probabilities <- model %>% predict(newx = features_test_matrix)
 labels_pred <- ifelse(probabilities > 0.5, 1, 0)
 #Evaluation Metrics
 Accuracy <- accuracy(labels_test,labels_pred)
-Accuracy
 precision <- posPredValue(as.factor(labels_pred), as.factor(labels_test), positive="1")
 recall <- sensitivity(as.factor(labels_pred), as.factor(labels_test), positive="1")
+Accuracy
 precision
 recall
 
-# Accuracy - 81.74%
-# Precision - 86.3%
-# Recall - 75.34%
+# Accuracy - 80.99%
+# Precision - 86.02%
+# Recall - 74%
+
+#Logistic Regression with Ridge Regularization
+cv.ridge <- cv.glmnet(features_train_matrix, labels_train_matrix, alpha = 0, family = "binomial")
+model <- glmnet(features_train_matrix, labels_train_matrix, alpha = 0, family = "binomial",
+                lambda = cv.ridge$lambda.min)
+probabilities <- model %>% predict(newx = features_test_matrix)
+labels_pred <- ifelse(probabilities > 0.5, 1, 0)
+#Evaluation Metrics
+Accuracy <- accuracy(labels_test,labels_pred)
+precision <- posPredValue(as.factor(labels_pred), as.factor(labels_test), positive="1")
+recall <- sensitivity(as.factor(labels_pred), as.factor(labels_test), positive="1")
+Accuracy
+precision
+recall
+
+# Accuracy - 80.7%
+# Precision - 86.46%
+# Recall - 72.8%
 
 
 ## Logistic model without regularization
 logistic_model <- glm(sentiment ~ ., 
                       data = training_set, 
                       family = "binomial")
-logistic_model
 predict_reg <- predict(logistic_model, 
                        testing_data, type = "response")
 predict_reg <- ifelse(predict_reg >0.5, 1, 0)
-
 #Evaluation Metrics
 Accuracy <- accuracy(labels_test,predict_reg)
-Accuracy
 precision <- posPredValue(as.factor(predict_reg), as.factor(labels_test), positive="1")
 recall <- sensitivity(as.factor(predict_reg), as.factor(labels_test), positive="1")
+Accuracy
 precision
 recall
 
-# Accuracy - 82.87%
-# Recall - 84.18%
-# Precision - 82.03%
+# Accuracy - 82.16%
+# Precision - 81.25%
+# Recall - 83.6%
 
 
 ## Random Forest Classifier
 pacman::p_load(randomForest)
-
 rf <- randomForest(
   sentiment ~ .,
   data=training_set,
   ntree = 200,
   sampsize = 0.80*nrow(training_set)
 )
-
-print(rf)
+#print(rf)
 y_pred = predict(rf, newdata = features_test)
 Accuracy <- accuracy(labels_test,y_pred)
-Accuracy
 precision <- posPredValue(as.factor(y_pred), as.factor(labels_test), positive="1")
 recall <- sensitivity(as.factor(y_pred), as.factor(labels_test), positive="1")
+Accuracy
 precision
 recall
 
@@ -219,35 +215,42 @@ n <- neuralnet(
   err.fct = "ce",
   linear.output = FALSE
 )
-
-output <- compute(n, features_test)
+output <- neuralnet::compute(n, features_test)
 p1 <- output$net.result
 pred <- ifelse(p1>0.5, 1, 0)
 pred
+Accuracy <- accuracy(labels_test, pred)
+precision <- posPredValue(as.factor(pred), as.factor(testing_data$sentiment), positive="1")
+recall <- sensitivity(as.factor(pred), as.factor(testing_data$sentiment), positive="1")
+Accuracy
+precision
+recall
+
+
 
 tab1 <- table(pred, testing_data$sentiment)
 tab1
 
+# Accuracy - 82.34%
+# Recall - 83.74%
+# Precision - 81.45%              
+
+pacman::p_load(rpart)
+
+
+# Decision Tree 
+training_set$sentiment <- factor(training_set$sentiment)
+tree <- rpart(sentiment ~., data = training_set)
+testing_data$sentiment <- factor(testing_data$sentiment)
+pred <- predict(tree, testing_data, type="class")
 Accuracy <- accuracy(labels_test, pred)
-precision <- posPredValue(as.factor(pred), as.factor(testing_data$sentiment), positive="1")
-recall <- sensitivity(as.factor(pred), as.factor(testing_data$sentiment), positive="1")
+precision <- posPredValue(as.factor(pred), as.factor(labels_test), positive="1")
+recall <- sensitivity(as.factor(pred), as.factor(labels_test), positive="1")
+Accuracy
 precision
 recall
-Accuracy
 
-
-# Accuracy - 82.83%
-# Recall - 84.08%
-# Precision - 82.02%              
-
-
-pacman::p_load(dplyr)
-
-
-pacman::p_load(wordcloud)
-
-
-
-View(final_reviews)
-
+# Accuracy - 71.03%
+# Precision - 67.13%
+# Recall - 82.38%
 
